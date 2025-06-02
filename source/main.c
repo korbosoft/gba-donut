07c40842d5e9f34d894f1525bbcdb5f970f2e64c
Korbo <korbin.m.marshall@gmail.com>
2025-06-01 21:56:30 -0500

    v1.1

--- source/main.c
+++ source/main.c
@@ -28,9 +28,6 @@ int main(void) {
 	BG_COLORS[0]=RGB8(0,0,0);
 	BG_COLORS[241]=RGB5(31,31,31);
 	SetMode(MODE_0 | BG0_ON);
-	// ansi escape sequence to clear screen and home cursor
-	// /x1b[line;columnH
-	// iprintf("\x1b[2J");
 
 	// initialise maxmod with soundbank and 8 channels
     mmInitDefault( (mm_addr)soundbank_bin, 4 );
@@ -38,34 +35,20 @@ int main(void) {
 	// Start playing module
 	mmStart( MOD_MUSIC, MM_PLAY_LOOP );
 
-	// ansi escape sequence to clear screen and home cursor
-	// /x1b[line;columnH
-	// iprintf("\x1b[2J");
-
-	// ansi escape sequence to set print co-ordinates
-	// /x1b[line;columnH
-	// float A = 0, B = 0;
-	// float i, j;
-	// int k;
-	// float z[1760];
-	// char b[1760];
 	int i;
 	short previousLine = 0;
 	char donutBuffer[COLUMNS + 1];
 	iprintf("\x1b[14;0H"
 			".----------------------------." \
-			"| GBA Donut v1.0             |" \
+			"| GBA Donut v1.1             |" \
 			"| Ported (???) by Korbo      |" \
 			"| Music by Jogeir Liljedahl  |" \
-			"|     (Press START to exit.) |" \
+			"|    (Turn off GBA to exit.) |" \
 			"'----------------------------'");
 	do {
 		for (i = 0; i < FRAMES * ROWS; ++i) {
 			memcpy(donutBuffer, donut + i * COLUMNS, COLUMNS + 1);
 			donutBuffer[COLUMNS] = '\0';
-			scanKeys();
-			if (keysDown() && KEY_START)
-				Stop();
 			if (i % (ROWS + 1) == 0)
 				iprintf("\x1b[0;0H");
 			iprintf("        %s", donutBuffer);
